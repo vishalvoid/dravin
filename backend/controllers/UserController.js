c2a001e78aa200dbb23ea09e08260cede3c68d89
Vishal Kumar Singh <kvishal07860@gmail.com>
2023-03-16 23:20:57 +0530

    Fixed Frontend not rendering on cross origin

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -122,7 +122,7 @@ exports.login = async (req, res) => {
     const options = {
       expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      domain: "dravin.cf",
+      domain: "https://dravin.cf",
       secure: true,
       sameSite: "none",
     };
